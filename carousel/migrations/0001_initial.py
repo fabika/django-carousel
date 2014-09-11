9c0dc8952bc4349a3c5f45c874d58c89ea714239
sm <dmitry.puhov@gmail.com>
2014-09-11 10:43:49 +0400

    Поддержка Django 1.7

--- carousel/migrations/0001_initial.py
+++ carousel/migrations/0001_initial.py
@@ -1,57 +1,48 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Carousel'
-        db.create_table(u'carousel_carousel', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=50)),
-            ('distribution', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=1)),
-        ))
-        db.send_create_signal(u'carousel', ['Carousel'])
+    dependencies = [
+    ]
 
-        # Adding model 'CarouselElement'
-        db.create_table(u'carousel_carouselelement', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('carousel', self.gf('django.db.models.fields.related.ForeignKey')(related_name='elements', to=orm['carousel.Carousel'])),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
-            ('url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
-            ('position', self.gf('django.db.models.fields.PositiveIntegerField')(default=1)),
-        ))
-        db.send_create_signal(u'carousel', ['CarouselElement'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Carousel'
-        db.delete_table(u'carousel_carousel')
-
-        # Deleting model 'CarouselElement'
-        db.delete_table(u'carousel_carouselelement')
-
-
-    models = {
-        u'carousel.carousel': {
-            'Meta': {'ordering': "('name',)", 'object_name': 'Carousel'},
-            'distribution': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'})
-        },
-        u'carousel.carouselelement': {
-            'Meta': {'ordering': "('position', 'name')", 'object_name': 'CarouselElement'},
-            'carousel': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'elements'", 'to': u"orm['carousel.Carousel']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'position': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1'}),
-            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['carousel']
+    operations = [
+        migrations.CreateModel(
+            name='Carousel',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('slug', models.SlugField(unique=True, verbose_name='slug')),
+                ('name', models.CharField(unique=True, max_length=50, verbose_name='name')),
+                ('distribution', models.PositiveSmallIntegerField(default=1, verbose_name='distribution', choices=[(1, 'sequential'), (2, 'random'), (3, 'weighted random'), (4, 'cluster random')])),
+            ],
+            options={
+                'ordering': ('name',),
+                'verbose_name': 'carousel',
+                'verbose_name_plural': 'carousels',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='CarouselElement',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50, verbose_name='name')),
+                ('image', models.ImageField(upload_to=b'carousel_uploads', verbose_name='image')),
+                ('url', models.URLField(verbose_name='URL', blank=True)),
+                ('position', models.PositiveIntegerField(default=1, help_text="The position of the element in the sequence or the weight of the element in the randomization process (depending on the carousel's distribution).", verbose_name='position')),
+                ('published', models.BooleanField(default=1, verbose_name='published')),
+                ('start_date', models.DateTimeField(help_text='If this field is filled show starts with a specified date', null=True, verbose_name='start date', blank=True)),
+                ('end_date', models.DateTimeField(help_text='If this field is filled show ending with a specified date', null=True, verbose_name='end date', blank=True)),
+                ('carousel', models.ForeignKey(related_name=b'elements', verbose_name='carousel', to='carousel.Carousel')),
+            ],
+            options={
+                'ordering': ('position', 'name'),
+                'verbose_name': 'carousel element',
+                'verbose_name_plural': 'carousel elements',
+            },
+            bases=(models.Model,),
+        ),
+    ]
